42a471c5b87cb9c9f4c2622304d7fce56f679016
Thesker <theasker.gmail.com>
2013-09-28 08:35:43 +0200

    Tarea 3: comenzar bisiesTO

--- DWEC03/tarea_dwec03.js
+++ DWEC03/tarea_dwec03.js
@@ -42,27 +42,54 @@ function varios(){
         nomApe=window.prompt("El nombre no puede estar en blanco\nIntroduzca su nombre y apellidos");
     }
     */
-    var fechaNac = window.prompt("Introduce la fecha que naciste (dd/mm/aa)");    
-    while(!(validarFecha(fechaNac)) || isNaN(dia) || isNaN(mes)|| isNaN(anio)){
-        fechaNac = window.prompt("No has introducido una fecha válida.\nIntroduce la fecha que naciste (dd/mm/aa)");
-    }
-
-}
-
-// validación de la fecha introducida
-function validarFecha(fechaNac){
+    var fechaNac = window.prompt("Introduce la fecha que naciste (dd-mm-aa)");
     var fecha = fechaNac.split('/');
     var dia = parseInt(fecha[0]);
     var mes = parseInt(fecha[1]);
     var anio = parseInt(fecha[2]);
+    while(!(validarFecha(dia,mes,anio)) || isNaN(dia) || isNaN(mes)|| isNaN(anio)){
+        fechaNac = window.prompt("No has introducido una fecha válida.\nIntroduce la fecha que naciste (dd/mm/aa)");
+    }
+    alert("comprobación final");
+    document.write((!(validarFecha(dia,mes,anio)) || isNaN(dia) || isNaN(mes)|| isNaN(anio)));
+}
+
+function validarFecha(dia,mes,anio){
     var correcto = false;
-    var ahora = new Date();
-    if(dia<1 || dia>31 || mes<1 || mes>12 || anio<1 || anio>ahora.getFullYear()){
+    var fecha = new Date();
+    var diasMes = new Array();
+    var errores = "";
+    diasMes[1] = 31;
+    diasMes[2] = 29;
+    diasMes[3] = 31;
+    diasMes[4] = 30;
+    diasMes[5] = 31;
+    diasMes[6] = 30;
+    diasMes[7] = 31;
+    diasMes[8] = 31;
+    diasMes[9] = 30;
+    diasMes[10] = 31;
+    diasMes[11] = 30;
+    diasMes[12] = 31;
+    alert("voy a comprobar");
+    if(dia<1 || dia>31 || mes<1 || mes>12 || anio<1 || anio>fecha.getFullYear()){
         correcto = false;
+        errores = "NO está dentro del margen de dias de los meses\n";
+        alert("comprobado y false");
     }else{
         correcto = true;
-        bisiesto(dia,mes,anio);
+        errores += "está dentro del margen de dias de los meses\n";
+        alert("dia:"+dia+"diasMes["+mes+"]:"+diasMes[mes]+".."+(dia>diasMes[mes]));
+        // Comprobamos que el dia está dentro del margen de los días de cada mes
+        if(dia>diasMes[mes]){
+            correcto = false;
+            errores += "el mes "+mes+" no tiene "+dia+" días\n";
+            alert("comprobado3");
+        }
     }
+    alert("comprobacion 4");
+    errores += errores+"("+correcto+")";
+    alert(errores);
     return correcto;
 }
 
