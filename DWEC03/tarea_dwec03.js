4b914966b9756434afa5e4c8d6796986c3aac87f
Thesker <theasker.gmail.com>
2013-09-30 11:56:08 +0200

    tarea 3 (inicio de control de año bisiesto)

--- DWEC03/tarea_dwec03.js
+++ DWEC03/tarea_dwec03.js
@@ -50,48 +50,41 @@ function varios(){
     while(!(validarFecha(dia,mes,anio)) || isNaN(dia) || isNaN(mes)|| isNaN(anio)){
         fechaNac = window.prompt("No has introducido una fecha válida.\nIntroduce la fecha que naciste (dd/mm/aa)");
     }
-    alert("comprobación final");
-    document.write((!(validarFecha(dia,mes,anio)) || isNaN(dia) || isNaN(mes)|| isNaN(anio)));
+    
 }
 
 function validarFecha(dia,mes,anio){
     var correcto = false;
     var fecha = new Date();
-    var diasMes = new Array();
+    var diasMes = new Array(0,31,29,31,30,31,30,31,31,30,31,30,31);
     var errores = "";
-    diasMes[1] = 31;
-    diasMes[2] = 29;
-    diasMes[3] = 31;
-    diasMes[4] = 30;
-    diasMes[5] = 31;
-    diasMes[6] = 30;
-    diasMes[7] = 31;
-    diasMes[8] = 31;
-    diasMes[9] = 30;
-    diasMes[10] = 31;
-    diasMes[11] = 30;
-    diasMes[12] = 31;
     alert("voy a comprobar");
     if(dia<1 || dia>31 || mes<1 || mes>12 || anio<1 || anio>fecha.getFullYear()){
         correcto = false;
         errores = "NO está dentro del margen de dias de los meses\n";
-        alert("comprobado y false");
     }else{
         correcto = true;
         errores += "está dentro del margen de dias de los meses\n";
-        alert("dia:"+dia+"diasMes["+mes+"]:"+diasMes[mes]+".."+(dia>diasMes[mes]));
         // Comprobamos que el dia está dentro del margen de los días de cada mes
         if(dia>diasMes[mes]){
             correcto = false;
             errores += "el mes "+mes+" no tiene "+dia+" días\n";
-            alert("comprobado3");
+        }else if(mes == 2 && dia == 29){ // El día es correcto, a falta de comprobar si es bisiesto si es 29/02
+            if (bisiesto(anio)){
+                correcto = true;
+            }else{
+                correcto = false;
+            }
         }
     }
-    alert("comprobacion 4");
     errores += errores+"("+correcto+")";
-    alert(errores);
     return correcto;
+    function bisiesto(anioBis){
+        return true;
+    }
 }
+
+
 
 function cerrarNueva() {
   if (nuevaVentana) {
