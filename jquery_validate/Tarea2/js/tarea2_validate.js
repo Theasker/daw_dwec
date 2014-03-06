d178a266130e71b33a21acad806bdd7a01091ebb
Theasker <Theasker@gmail.com>
2014-03-07 00:10:57 +0100

    Tarea 2 - Validación de formularios

---
+++ jquery_validate/Tarea2/js/tarea2_validate.js
@@ -0,0 +1,16 @@
+$("#formulario").validate({
+  onkeyup: false,
+  onfocusout: false,
+  onclick: false,
+  rules: {
+    nombre: {
+      required: true,
+      minlength: 2
+    },
+    email: "required",
+    comentarios: "required",
+    cuenta: "cuentabanco"
+  }
+});
+
+
